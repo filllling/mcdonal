a892072173b958470dca0773fda6c9dad20716d8
filllling <infilling@qq.com>
2025-06-20 23:56:45 +0800

    5

--- run.py
+++ run.py
@@ -3,4 +3,5 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # 默认5000，本地调试时仍可工作
+    app.run(host="0.0.0.0", port=port, debug=True)
